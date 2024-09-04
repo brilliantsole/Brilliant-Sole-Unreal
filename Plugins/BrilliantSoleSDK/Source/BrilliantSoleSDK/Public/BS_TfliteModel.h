26703dc309dde827a1d8fdded134a2a9b7a1a32b
Zack <zack@ukaton.com>
2024-09-03 18:05:22 -0700

    .

---
+++ Plugins/BrilliantSoleSDK/Source/BrilliantSoleSDK/Public/BS_TfliteModel.h
@@ -0,0 +1,21 @@
+// Copyright 2024 Zack Qattan @ Brilliant Sole. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BS_TfliteModel.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogBS_TfliteModel, Log, Warning);
+
+UCLASS(Blueprintable)
+class BRILLIANTSOLESDK_API UBS_TfliteModel : public UObject
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintPure, Category = "BS Tflite Model")
+    static const TArray<uint8> &GetTfliteModel() { return TfliteModel; }
+
+private:
+    static const TArray<uint8> TfliteModel;
+};
